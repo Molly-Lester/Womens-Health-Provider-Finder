26fe676ae67b7c880435b03363613e86f620f48c
destinyiyamu13-oss <destinyiyamu13@gmail.com>
2026-06-17 00:31:29 +0100

    Improve search form and resutls page layout

--- src/pages/ResultsPage.jsx
+++ src/pages/ResultsPage.jsx
@@ -1,3 +1,36 @@
-export default function ResultsPage({ searchData }) {
+import { useNavigate } from "react-router-dom";
 
+export default function ResultsPage({ searchData }) {
+    const navigate = useNavigate();
+
+
+    if(!searchData) {
+        return(
+            <div className= "results-empty">
+                <h2>No search found</h2>
+                <p>Please complete a search to view nearby services.</p>
+                
+                <button onClick={() => navigate("/")}>
+                    Back to search
+                </button>
+            </div>
+        );
+    }
+   
+    return(
+        <div>
+            <h2>Search Results</h2>
+
+            <p>Location: {searchData.location}</p>
+            <p>Distance: {searchData.radius}</p>
+            <p>Category: {searchData.category}</p>
+            <p>Provider: {searchData.providerType}</p>
+
+            <h3>Clinics</h3>
+
+            <button onClick={() => navigate("/")}>
+                New Search
+            </button>
+        </div>
+    )
 }
